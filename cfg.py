d89c67bce5d212860fb740f38f8ded416193c43f
Bobby Wisdom <bobby.wisdom@gmail.com>
2016-05-19 15:09:33 -0500

    Updates
    Merged all config files into single cfg.py.

--- cfg.py
+++ cfg.py
@@ -1,6 +1,55 @@
 #cfg.py
+import string
+import socket
+
 HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "resinbot"
 PASS = "oauth:jgt7f9oa7laa6321o98innxwetwkzo"
 CHAN = "resinplays"
+
+def open_socket():
+	s = socket.socket()
+	s.connect((HOST, PORT))
+	s.send(bytes("PASS " + PASS + "\r\n", "UTF-8"))
+	s.send(bytes("NICK " + NICK + "\r\n", "UTF-8"))
+	s.send(bytes("JOIN #" + CHAN + "\r\n", "UTF-8"))
+	return s
+
+def join_room(s):
+	readbuffer = ""
+	loading = True
+	while loading:
+		readbuffer = readbuffer + s.recv(1024).decode("UTF-8")
+		temp = str.split(readbuffer, "\n")
+		readbuffer = temp.pop()
+
+		for line in temp:
+			print(line)
+			loading = loading_complete(line)
+	send_message(s, "Successfully joined chat")
+
+
+def loading_complete(line):
+	if("End of /NAMES list" in line):
+		return False
+	else:
+		return True
+
+def get_user(line):
+	separate = line.split(":", 2)
+	user = separate[1].split("!", 1)[0]
+	return user
+
+def get_message(line):
+	separate = line.split(":", 2)
+	if separate[0] == "PING ":
+		s.send(bytes("PONG :tmi.twitch.tv\r\n".encode("UTF-8")))
+	else:
+		message = separate[2]
+	return message
+
+def send_message(s, message):
+	message_temp = "PRIVMSG #" + CHAN + " :" + message
+	s.send(bytes(message_temp + "\r\n", "UTF-8"))
+	print("Sent: " + message_temp)
--- main.py
+++ main.py
@@ -9,15 +9,15 @@ Sub notification in chat
 '''
 
 import string
-from socket_cfg import *
-from initialize import join_room
-from read import *
+from cfg import *
 
 s = open_socket()
 join_room(s)
 readbuffer = ""
 temp_list = ""
 commands = ["!songrequest", "!song", "!mods"]
+added_commands = []
+added_commands_text = []
 
 while True:
 	readbuffer = readbuffer + s.recv(1024).decode("UTF-8")
@@ -28,10 +28,35 @@ while True:
 		print(line)
 		user = get_user(line)
 		message = get_message(line)
-		print(user + " typed :" + message)
-		if "You Suck" in message:
-			send_message(s, "No, you suck")
-		if "!commands" in message:
-			for command in commands:
+		analysis = message.split(" ")
+		#print(user + " typed :" + message)
+
+		#if "!commands" in message:
+		if analysis[0] == "!commands":
+			merged_lists = commands + added_commands
+			for command in merged_lists:
 				temp_list += str(command) + ", "
 			send_message(s, "These are channel commands: " + temp_list)
+
+		#if "!addcomm" in message && user == CHAN:
+		elif analysis[0] == "!addcomm" && user == CHAN:
+			breakdown = message.split(" +")
+			added_commands.extend[breakdown[1]]
+			added_commands_text.extend[breakdown[2]]
+
+		#if "!songrequest" in message:
+		elif analysis[0] == "!songrequest":
+			#take URL and add it to a list for playback
+			#2 part list split by space
+			#once played remove from list
+			#blacklist? How?
+			#guard against evil requests
+
+		#if "!song" in message:
+		elif analysis[0] == "!song":
+
+		elif analysis[0] == "!pausebot" && user == CHAN:
+			#put bot to sleep
+			#if we do this how do we restart it?
+
+		elif analysis[0] == "!startbot" && user == CHAN:
